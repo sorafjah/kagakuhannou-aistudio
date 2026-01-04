3c65c4977ccd3e72ba8d8de3a047e6d6313662b3
そらふや <tofya17@gmail.com>
2026-01-04 23:18:33 +0900

    refactor: Remove React dependency and use Vanilla JS
    The application has been refactored to use Vanilla JavaScript and is now served directly from index.html. This change simplifies the build process and removes unnecessary React dependencies.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,3 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Root element not found");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// このファイルは不要になりました。ロジックは index.html に統合されています。
+console.log("Application is running from index.html (Vanilla JS Mode)");
